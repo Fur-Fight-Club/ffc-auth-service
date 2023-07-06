2dc3e4b2375adee98fe9e242ceff94dd8468a735
Milan <pro@turtletv.fr>
2023-07-06 11:55:08 +0200

    ✅ Fix tests

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { UserService } from "./user.service";
+import { AuthService } from "src/auth/auth.service";
+import { PrismaService } from "src/services/prisma.service";
+import { UsersRepository } from "./user.repository";
+import { JwtService } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
 
 describe("UserService", () => {
   let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        PrismaService,
+        AuthService,
+        UsersRepository,
+        JwtService,
+        ConfigService,
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
